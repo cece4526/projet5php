a598a59e9ca51053e221ca776904efb5366de929
Cédric Dupré <cece4526@hotmail.fr>
2023-05-11 09:16:13 +0200

    create controller

---
+++ src/controller/FrontController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\src\controller;
+use App\config\Parameter;
+
+class FrontController extends Controller{
+    public function home(){
+        $articles = $this->articleDAO->getAllArticles();
+        return $this->view->render('home', [
+           'allArticles' => $articles
+        ]);
+    }
+
+    public function article($articleId){
+        $article = $this->articleDAO->getOneArticle($articleId);
+        $comments = $this->commentDAO->getCommentsFromArticle($articleId);
+        return $this->view->render('single', [
+            'article' => $article,
+            'allComments' => $comments
+        ]);
+    }
+    public function addComment(Parameter $post, $articleId)
+    {
+        if($post->get('submit')) {
+            $errors = $this->validation->validate($post, 'Comment');
+            if(!$errors) {
+                $this->commentDAO->addComment($post, $articleId);
+                $this->session->set('add_comment', 'Le nouveau commentaire a bien été ajouté');
+                header('Location: ../public/index.php');
+            }
+            $article = $this->articleDAO->getOneArticle($articleId);
+            $comments = $this->commentDAO->getCommentsFromArticle($articleId);
+            return $this->view->render('single', [
+                'article' => $article,
+                'comments' => $comments,
+                'post' => $post,
+                'errors' => $errors
+            ]);
+        }
+    }
+    public function flagComment($commentId){
+        $this->commentDAO->flagComment($commentId);
+        $this->session->set('flag_comment', 'Le commentaire a bien été signalé');
+        header('Location: ../public/index.php');
+    }
+    public function register(Parameter $post)
+    {
+        if($post->get('submit')) {
+            $errors = $this->validation->validate($post, 'User');
+            if($this->userDAO->checkUser($post)) {
+                $errors['pseudo'] = $this->userDAO->checkUser($post);
+            }
+            if(!$errors) {
+                $this->userDAO->register($post);
+                $this->session->set('register', 'Votre inscription a bien été effectuée');
+                header('Location: ../public/index.php');
+            }
+            return $this->view->render('register', [
+                'post' => $post,
+                'errors' => $errors
+            ]);
+
+        }
+        return $this->view->render('register');
+    }
+    public function login(Parameter $post){
+        if($post->get('submit')) {
+            $user = $this->userDAO->login($post);
+            $result = $this->checkLogin($post, $user);
+            if($result && $result['isPasswordValid']) {
+                $this->session->set('login', 'Content de vous revoir');
+                $this->session->set('id', $result['result']['id']);
+                $this->session->set('role', $result['result']['name']);
+                $this->session->set('pseudo', $post->get('pseudo'));
+                header('Location: ../public/index.php');
+            }
+            else {
+                $this->session->set('error_login', 'Le pseudo ou le mot de passe sont incorrects');
+                return $this->view->render('login', [
+                    'post'=> $post
+                ]);
+            }
+        }
+        return $this->view->render('login');
+    }
+    public function checkLogin(Parameter $post, $result){
+        if ($result !== false){
+            $isPasswordValid = password_verify($post->get('password'), $result['password']);
+            return [
+                'result' => $result,
+                'isPasswordValid' => $isPasswordValid
+            ];
+        }
+        return [
+            'result' => false,
+            'isPasswordValid' => false
+        ];
+    }
+}
